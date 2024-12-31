658a1444a7f26085049adeab8b65777df4400c60
juliensobrier <jsobrier@blitapp.com>
2024-12-31 15:36:11 -0800

    Reformat tests

--- src/LanguageDetector.test.ts
+++ src/LanguageDetector.test.ts
@@ -17,26 +17,20 @@ describe('LanguageDetector', () => {
 
 
 
-    const data = fs.readdirSync('testdata', { withFileTypes: true })
-    .filter((dirent : any) => dirent.isDirectory())
-    .map((dirent : any) => dirent.name)
+    // Test for all languages in testdata
+    const path = 'testdata';
+    const folders = fs.readdirSync(path, { withFileTypes: true })
+        .filter((dirent : any) => dirent.isDirectory())
+        .map((dirent : any) => dirent.name)
 
-    data.forEach((language : string) => {
+    folders.forEach((language : string) => {
         test(`should detect ${language} language`, () => {
-            const text = fs.readFileSync(`testdata/${language}/short.txt`, 'utf8');
+            const text = fs.readFileSync(`${path}/${language}/short.txt`, 'utf8');
             const results = detector.getLanguages(text);
-            console.log(results);
             expect(results[0]).toBe(language);
         });
     });
 
-
-
-    test('should return null for unknown language', () => {
-        const text = 'Dies ist ein deutscher Text.';
-        const results = detector.getLanguages(text);
-        expect(results[0]).toBe('de');
-    });
 
     test('should return null for empty text', () => {
         const text = '';
